c9818a6077f11c6b745e112f8e84114cf8eee0f8
Luis Grangeiro <luiscarlos.grangeiro@gmail.com>
2021-10-13 02:02:21 -0300

    adiciona pacote com as matrizes de transformação

---
+++ matrizes.py
@@ -0,0 +1,22 @@
+import numpy as np
+
+def matriz_rotacao(theta_rad):
+    return np.array([[np.cos(theta_rad), -np.sin(theta_rad), 0],
+                               [np.sin(theta_rad),  np.cos(theta_rad), 0],
+                               [0, 0, 1]])
+
+def matriz_escala(cx, cy):
+    return np.array([[cx, 0, 0],
+                     [0, cy, 0],
+                     [0, 0, 1]])
+
+def matriz_translacao_origem(xcentro, ycentro):
+    return np.array([[1, 0, -xcentro],
+                     [0, 1, -ycentro],
+                     [0, 0, 1]])
+
+def matriz_translacao_volta(xcentro, ycentro):
+    return  np.array([[1, 0, xcentro],
+                      [0, 1, ycentro],
+                      [0, 0, 1]])
+
